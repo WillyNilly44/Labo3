76d62c5643405a0e7975d77c6f90e1f3cf35eae4
WillyNilly44 <94925679+WillyNilly44@users.noreply.github.com>
2023-04-15 19:28:45 -0400

    yes

---
+++ Labo3 - Copy/Controllers/AccountsController.cs
@@ -0,0 +1,130 @@
+﻿using MDB.Models;
+using MailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Web.Mvc;
+
+namespace MDB.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly AppDBEntities DB = new AppDBEntities();
+
+        [HttpPost]
+        public JsonResult EmailExist(string email)
+        {
+            return Json(DB.EmailExist(email));
+        }
+        [HttpPost]
+        public JsonResult EmailAvailable(string email,int id)
+        {
+            return Json(DB.EmailAvailable(email,id));
+        }
+
+        #region Login and Logout
+        public ActionResult Login(string message)
+        {
+            ViewBag.Message = message;
+            return View(new LoginCredential());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Login(LoginCredential loginCredential)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DB.EmailBlocked(loginCredential.Email))
+                {
+                    ModelState.AddModelError("Email", "Ce compte est bloqué.");
+                    return View(loginCredential);
+                }
+                if (!DB.EmailVerified(loginCredential.Email))
+                {
+                    ModelState.AddModelError("Email", "Ce courriel n'est pas vérifié.");
+                    return View(loginCredential);
+                }
+                User user = DB.GetUser(loginCredential);
+                if (user == null)
+                {
+                    ModelState.AddModelError("Password", "Mot de passe incorrecte.");
+                    return View(loginCredential);
+                }
+                if (OnlineUsers.IsOnLine(user.Id))
+                {
+                    ModelState.AddModelError("Email", "Cet usager est déjà connecté.");
+                    return View(loginCredential);
+                }
+                OnlineUsers.AddSessionUser(user.Id);
+                Session["currentLoginId"] = DB.AddLogin(user.Id).Id;
+                return RedirectToAction("Index", "Movies");
+            }
+            return View(loginCredential);
+        }
+        public ActionResult Logout()
+        {
+            if (Session["currentLoginId"] != null)
+                DB.UpdateLogout((int)Session["currentLoginId"]);
+            OnlineUsers.RemoveSessionUser();
+            return RedirectToAction("Login");
+        }
+        #endregion
+
+        #region Subscribe
+        public ActionResult Subscribe()
+        {
+            ViewBag.Genres = SelectListUtilities<Gender>.Convert(DB.Genders.ToList(), "Name");
+            return View(new User());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public ActionResult Subscribe(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DB.AddUser(user) != null)
+                    return RedirectToAction("Login");
+                else
+                    return RedirectToAction("Report", "Errors", new { message = "Échec de création de compte" });
+            }
+            ViewBag.Genres = SelectListUtilities<Gender>.Convert(DB.Genders.ToList(), "Name");
+            return View(user);
+        }
+
+        public void SendEmailVerification(User user, string newEmail)
+        {
+            if (user.Id != 0)
+            {
+                UnverifiedEmail unverifiedEmail = DB.Add_UnverifiedEmail(user.Id, newEmail);
+                if (unverifiedEmail != null)
+                {
+                    string verificationUrl = Url.Action("VerifyUser", "Accounts", null, Request.Url.Scheme);
+                    String Link = @"<br/><a href='" + verificationUrl + "?userid=" + user.Id + "&code=" + unverifiedEmail.VerificationCode + @"' > Confirmez votre inscription...</a>";
+
+                    String suffixe = "";
+                    if (user.GenderId == 2)
+                    {
+                        suffixe = "e";
+                    }
+                    string Subject = "MDB - Vérification d'inscription...";
+
+                    string Body = "Bonjour " + user.GetFullName(true) + @",<br/><br/>";
+                    Body += @"Merci de vous être inscrit" + suffixe + " au site MDB. <br/>";
+                    Body += @"Pour utiliser votre compte vous devez confirmer votre inscription en cliquant sur le lien suivant : <br/>";
+                    Body += Link;
+                    Body += @"<br/><br/>Ce courriel a été généré automatiquement, veuillez ne pas y répondre.";
+                    Body += @"<br/><br/>Si vous éprouvez des difficultés ou s'il s'agit d'une erreur, veuillez le signaler à <a href='mailto:"
+                         + SMTP.OwnerEmail + "'>" + SMTP.OwnerName + "</a> (Webmestre du site MDB)";
+
+                    SMTP.SendEmail(user.GetFullName(), unverifiedEmail.Email, Subject, Body);
+                }
+            }
+        }
+
+        #endregion
+
+
+    }
+}
